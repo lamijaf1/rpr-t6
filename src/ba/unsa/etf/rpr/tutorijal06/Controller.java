95c30cbace42afa02a72c126fa0dcbc9ed3c34c5
Lamija <lfazlija1@etf.unsa.ba>
2018-11-26 00:00:20 +0100

    dodane metode validno ime, prezime i formularvalidan

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -1,6 +1,24 @@
 package ba.unsa.etf.rpr.tutorijal06;
 
-public class Controller {
+import javafx.scene.control.TextField;
 
+public class Controller {
+    public TextField imeField;
+    public TextField prezimeField;
+    public TextField indeksField;
+    private boolean imeValidno;
+    private boolean prezimeValidno;
+    private boolean indeksValidan;
+
+    private boolean validnoIme(String n) {
+        return !n.trim().isEmpty();
+    }
+    private boolean validnoPrezime(String n) {
+        return !n.trim().isEmpty();
+    }
+
+    public boolean formularValidan() {
+        return imePrezimeValidno;
+    }
 
 }
