666b555750622130dd7852790167802ceec9f211
Lamija <lfazlija1@etf.unsa.ba>
2018-11-26 00:03:34 +0100

    dodana initialize metoda, kojom cemo da provjeravamo da li su uneseni validni podaci

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.tutorijal06;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -16,9 +19,33 @@ public class Controller {
     private boolean validnoPrezime(String n) {
         return !n.trim().isEmpty();
     }
-
+    private boolean validanIndeks(String n) {
+        return !n.trim().isEmpty();
+    }
     public boolean formularValidan() {
-        return imePrezimeValidno;
+        return (imeValidno&&prezimeValidno && indeksValidan);
+    }
+    @FXML
+    public void initialize() {
+        imeValidno = false;
+        prezimeValidno=false;
+        imeField.getStyleClass().add("poljeNijeIspravno");
+        prezimeField.getStyleClass().add("poljeNijeIspravno");
+
+       /* imePrezimeField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (validnoImePrezime(n)) {
+                    imePrezimeField.getStyleClass().removeAll("poljeNijeIspravno");
+                    imePrezimeField.getStyleClass().add("poljeIspravno");
+                    imePrezimeValidno = true;
+                } else {
+                    imePrezimeField.getStyleClass().removeAll("poljeIspravno");
+                    imePrezimeField.getStyleClass().add("poljeNijeIspravno");
+                    imePrezimeValidno = false;
+                }
+            }
+        });*/
     }
 
 }
