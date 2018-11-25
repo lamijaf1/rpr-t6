20a9fcae8e9c4007b8f711e9448319db4c8065c9
Lamija <lfazlija1@etf.unsa.ba>
2018-11-25 23:46:09 +0100

    napravljen osnovni izgled formulara

---
+++ src/ba/unsa/etf/rpr/tutorijal06/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.tutorijal06;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Formular");
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.setResizable(true);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
