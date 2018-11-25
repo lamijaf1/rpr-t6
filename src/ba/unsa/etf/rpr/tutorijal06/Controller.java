ad9f5136969687aadb4ed67f3431b815b9c5a558
Lamija <lfazlija1@etf.unsa.ba>
2018-11-26 00:52:22 +0100

    promjena boje kada se unese ime duze od 2 i krace od 20 karaktera

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal06;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -12,8 +13,24 @@ public class Controller {
     private boolean imeValidno;
     private boolean prezimeValidno;
     private boolean indeksValidan;
-
+    private SimpleStringProperty prezime;
+    public String getTekst() {
+        return prezime.get();
+    }
+    public Controller() {
+        prezime = new SimpleStringProperty("");
+    }
+    public SimpleStringProperty tekstProperty() {
+        return prezime;
+    }
+    public void setTekst(String tekst) {
+        this.prezime.set(tekst);
+    }
     private boolean validnoIme(String n) {
+        if(n.length()<2 || n.length()>20)return false;
+        for(int i=0;i<n.length();i++){
+            if(n.charAt(i)<'A'|| n.charAt(i)>'Ž' || n.charAt(i)<'a' ||  n.charAt(i)>'ž') return false;
+        }
         return !n.trim().isEmpty();
     }
     private boolean validnoPrezime(String n) {
@@ -50,7 +67,7 @@ public class Controller {
         prezimeField.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (validnoIme(n)) {
+                if (validnoPrezime(n)) {
                     prezimeField.getStyleClass().removeAll("poljeNijeIspravno");
                     prezimeField.getStyleClass().add("poljeIspravno");
                     prezimeValidno= true;
@@ -58,6 +75,20 @@ public class Controller {
                     prezimeField.getStyleClass().removeAll("poljeIspravno");
                     prezimeField.getStyleClass().add("poljeNijeIspravno");
                     prezimeValidno = false;
+                }
+            }
+        });
+        indeksField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (validanIndeks(n)) {
+                    indeksField.getStyleClass().removeAll("poljeNijeIspravno");
+                    indeksField.getStyleClass().add("poljeIspravno");
+                    indeksValidan= true;
+                } else {
+                    indeksField.getStyleClass().removeAll("poljeIspravno");
+                    indeksField.getStyleClass().add("poljeNijeIspravno");
+                    indeksValidan = false;
                 }
             }
         });
